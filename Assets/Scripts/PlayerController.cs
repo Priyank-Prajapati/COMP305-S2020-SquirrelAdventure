643d31cd07f37bcd9cf7b1a8939ba88e6e7a1333
Priyank <ppraja20@my.centennialcollege.ca>
2020-06-29 16:21:50 -0400

    Added Player Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,12 @@ public class PlayerController : MonoBehaviour
     //private variable
     private Rigidbody2D rBody;
     private bool isGrounded = false;
+    private Animator anim;
+    private bool isFacingRight = true;
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,17 +28,37 @@ public class PlayerController : MonoBehaviour
         //check if on ground
         isGrounded = GroundCheck();
         //jump code
-        if(isGrounded && Input.GetAxis("Jump") > 0)
+        if (isGrounded && Input.GetAxis("Jump") > 0)
         {
             rBody.AddForce(new Vector2(0.0f, jumpForce));
         }
 
         float horiz = Input.GetAxis("Horizontal");
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        //check if sprite needs to be flipped
+        if (isFacingRight && rBody.velocity.x < 0)
+            Flip();
+        else if (!isFacingRight && rBody.velocity.x > 0)
+            Flip();
+
+        //send value to animator
+        anim.SetFloat("xSpeed", Mathf.Abs(rBody.velocity.x)); //also horiz works
+        anim.SetFloat("ySpeed", Mathf.Abs(rBody.velocity.y));
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private bool GroundCheck()
     {
         return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+
+    void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
     }
 }
